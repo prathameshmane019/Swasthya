5d3e37a9a1ff4bfea61c48f7217789c2677bb90e
prathameshmane019 <maneprathamesh019@gmail.com>
2024-03-14 10:07:10 +0530

    conflicts resolved

--- client/swasthyasamridhhi/app/(main)/admin/page.js
+++ client/swasthyasamridhhi/app/(main)/admin/page.js
@@ -1,23 +1,6 @@
 import React from 'react'
 
 function page() {
- 
-
-
-<<<<<<< HEAD
-  const handleSubmit = async e => {
-    e.preventDefault();
-    try {
-      const response = await axios.post('/api/patient-register', formData);
-      console.log('Response:', response.data);
-    } catch (error) {
-      console.error('Error:', error.response.data);
-    }
-  };
-  
-=======
-
->>>>>>> parent of 4db298d (1.0.1)
   return (
     <div>
       this is admin page
